dc4b6ee57ec823edd905bc4f608e1444560be8ce
JeppeLeth <jeppe.leth@gmail.com>
2014-09-01 10:47:58 +0200

    Added Test

---
+++ BookKeeperTest/src/edu/kea/pm/bookkeeper/test/FirstTest.java
@@ -0,0 +1,14 @@
+package edu.kea.pm.bookkeeper.test;
+
+import junit.framework.TestCase;
+
+public class FirstTest extends TestCase
+{
+
+	
+	public void testGetBook()
+	{
+		fail("Not yet implemented");
+	}
+
+}
