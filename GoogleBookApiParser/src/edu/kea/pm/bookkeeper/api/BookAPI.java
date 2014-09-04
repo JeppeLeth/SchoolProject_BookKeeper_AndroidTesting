2c24ec82d7c5defa6dc3e695a76ac4d1cc226041
Rasmus Kartau <rasmus.kartau@gmail.com>
2014-09-04 12:23:21 +0200

    Testing Jenkins autobuild vol.2

--- GoogleBookApiParser/src/edu/kea/pm/bookkeeper/api/BookAPI.java
+++ GoogleBookApiParser/src/edu/kea/pm/bookkeeper/api/BookAPI.java
@@ -25,6 +25,7 @@ public class BookAPI {
     protected static final String KEY_DESCRIPTION = "description";
 
 
+
     public static Book readISBN(String isbn) throws IOException, JSONException {
         JSONObject readJsonFromUrl = JsonReader.readJsonFromUrl(GOOGLE_API_URL + isbn);
         return JsonParser.parseJson(readJsonFromUrl);
